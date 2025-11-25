89e965f28a37d18933fe6c79d14c902de6b1b5b5
Zahed Khan <mobilecity9@gmail.com>
2025-11-26 02:59:34 +0530

    initial full working project

---
+++ app/src/main/java/com/example/shareprint/MainActivity.java
@@ -0,0 +1,8 @@
+
+package com.example.shareprint;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override protected void onCreate(Bundle b){ super.onCreate(b); }
+}
---
+++ app/src/main/java/com/example/shareprint/ReceiveActivity.java
@@ -0,0 +1,8 @@
+
+package com.example.shareprint;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReceiveActivity extends Activity {
+    @Override protected void onCreate(Bundle b){ super.onCreate(b); }
+}
